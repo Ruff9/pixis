2a7ed55d43dc2a05a5dd3152b13a754f473d03dd
Remy Maucourt <remy.maucourt@yahoo.fr>
2018-05-20 18:39:45 +0200

    setup pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_05_19_183202) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "prospects", force: :cascade do |t|
     t.string "email"
     t.text "message"
