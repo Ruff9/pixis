9d7980426bb0b6dc76914a5e833aded1eeb7eddf
Remy Maucourt <remy.maucourt@yahoo.fr>
2018-06-12 22:36:08 +0200

    add header video

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  contactModal();
+  headerVideo();
+});
+
+var headerVideo = function() {
+  var video = document.querySelector('.header-video');
+  coverVid(video, 1920, 1080);
+}
+
+var contactModal = function() {
+  var modal = document.getElementById('contact-modal');
+  var btn = document.getElementById('contact-modal-trigger');
+  var span = document.getElementsByClassName("close")[0];
+
+  btn.onclick = function() {
+    modal.style.display = "flex";
+  }
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
